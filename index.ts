99dbabe5df9785a8a1b4cac402b72c373364a820
Icemourne <Cboweris@gmail.com>
2022-12-16 21:34:15 +0200

    update

--- index.ts
+++ index.ts
@@ -52,3 +52,32 @@ export function numArrToString(array: number[] | undefined): string {
    if (array === undefined) return ''
    return array.join(', ')
 }
+
+/**
+ ** Instead of regular stringify this one doesn't add new lines to specified arrays
+ ** Basically normal stringify would make
+ ** [
+ **   1,
+ **   2,
+ **   3
+ ** ]
+ ** This will make
+ ** [1, 2, 3]
+ */
+ export function customJsonStringify(object: object, properties: string[], spaces = 1) {
+   const string = JSON.stringify(object, undefined, spaces)
+   const regex = new RegExp(`"${properties.join('|')}"\\s*:\\s*\\[([^]+?)\\]`, 'g')
+
+   const matches = string.match(regex) // /"(stat|multiplier|weaponTypes)": \[[^]+?\]/g
+   if (matches === null) return string
+
+   const newString = matches.reduce((acc, match) => {
+      acc = acc.replace(match, match.replaceAll('\n', '').replace(/  +/g, ' '))
+      return acc
+   }, string)
+
+   return newString
+}
+
+// todo: add index DB
+// todo: add fetch with automatic repeat on fail // call it persistentFetch
