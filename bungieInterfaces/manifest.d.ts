e248238f109f32d7c6691106418933477003e12e
Icemourne <Cboweris@gmail.com>
2022-12-20 22:43:18 +0200

    clean up

--- bungieInterfaces/manifest.d.ts
+++ bungieInterfaces/manifest.d.ts
@@ -2,22 +2,27 @@ import { InventoryItem } from "./inventoryItem";
 import { PlugSet } from "./plugSet";
 import { SocketType } from "./socketType";
 import { Stat } from "./stat";
+
 export interface InventoryItems {
-    [key: string]: InventoryItem;
+   [key: string]: InventoryItem
 }
+
 export interface PlugSets {
-    [key: string]: PlugSet;
+   [key: string]: PlugSet
 }
+
 export interface SocketTypes {
-    [key: string]: SocketType;
+   [key: string]: SocketType
 }
+
 export interface Stats {
-    [key: string]: Stat;
+   [key: string]: Stat
 }
+
 export interface Manifest {
-    version: string;
-    inventoryItems?: InventoryItems;
-    plugSets?: PlugSets;
-    socketTypes?: SocketTypes;
-    stats?: Stats;
+   version:         string
+   inventoryItems?: InventoryItems
+   plugSets?:       PlugSets
+   socketTypes?:    SocketTypes
+   stats?:          Stats
 }
